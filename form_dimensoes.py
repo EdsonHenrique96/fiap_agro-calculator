5a68e22b0884340109abe75a35161725253bc7ea
Edson Henrique <henrique.ti1196@gmail.com>
2025-03-22 10:39:40 -0300

    aceitar somente numeros na largura e comprimento

--- form_dimensoes.py
+++ form_dimensoes.py
@@ -2,13 +2,35 @@ from utils import delimiter
 
 
 def get_largura():
-    largura = float(input("Digite a largura do terreno em metros: "))
-    return largura
+    while True:
+        largura = input("Digite a largura do terreno em metros: ")
+
+        if not largura.isnumeric():
+            print("\n***Valor inválido, digite um valor númerico!***\n")
+            continue
+        elif float(largura) <= 0:
+            print("\n***Valor deve ser maior que zero!***\n")
+            continue
+        else:
+            break
+
+    return float(largura)
 
 
 def get_comprimento():
-    comprimento = float(input("Digite o comprimento do terreno em metros: "))
-    return comprimento
+    while True:
+        comprimento = input("Digite o comprimento do terreno em metros: ")
+
+        if not comprimento.isnumeric():
+            print("\n***Valor inválido, digite um valor númerico!***\n")
+            continue
+        elif float(comprimento) <= 0:
+            print("\n***Valor deve ser maior que zero!***\n")
+            continue
+        else:
+            break
+
+    return float(comprimento)
 
 
 def form_dimensoes(dimensoes: list):
