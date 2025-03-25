fab991950423dd5adc6f0989b667c0509fc9887f
Edson Henrique <henrique.ti1196@gmail.com>
2025-03-25 13:57:01 -0300

    ajuste descrição de insumos

--- README.md
+++ README.md
@@ -8,9 +8,15 @@ Na cultura de café o plantio é realizado por fileiras, cada fileira contém um
 Para o calculo de insumos levamos em consideração somente a area útil (area útil = area - area_ruas)
 
 - Mudas: além do espaço entras as linhas(3,5m), cada planta tem um espaçamento de 0,8m entre si dentro da linha, resultando em 1 muda a cada 2,8m², logo consideramos a area total para realizar o calcúlo.
-- Calcário: 300g
-- Fertilizantes: 150g
-- Palhada: 5kg
+- Nitrogênio: 2g
+- Fósforo: 1g
+- Potássio: 2g
+- Calcário: 200g
+- Gesso Agrícola: 100g
+- Herbicida: 10 ml
+- Fungicida: 10 ml
+- Inseticida: 5 ml
+- Agua: 6 L por dia
 
 ### Calculo de area
 Figura geométrica: retângulo
@@ -22,12 +28,15 @@ Figura geométrica: retângulo
 Para o calculo de insumos levamos em consideração somente a area útil (area útil = area - area_ruas)
 
 - Mudas: Para soja é levado em consideração a densidade de plantio que varia entre 20 e 40 plantas por m2
-- Água: 4 a 7 litros por m²
-- Fosfato: 2g a 6g
-- Potássio: 2g a 5g
-- Nitrogênio: 0 a 2 g/m²
-- Calcário: 100 a 200 g/m²
-- Gesso agrícola: 50 a 100 g/m²
+- Calcário: 100g
+- Gesso Agrícola: 50g
+- Fosfato: 2g
+- Potássio: 2g
+- Nitrogênio: 1g
+- Herbicida: 10 ml
+- Fungicida: 10 ml
+- Inseticida: 5 ml
+- Água: 4 L por dia
 
 ---
 # Informações gerais
--- calculadora_insumos.py
+++ calculadora_insumos.py
@@ -3,25 +3,27 @@ from constantes import get_cultura
 
 
 def cal_insumos_soja(area_util, densidade):
+    mudas = densidade * area_util
     calcario = 0.1  # kg/m² (convertido de 100g)
     gesso = 0.05  # kg/m² (convertido de 50g)
     fosfato = 0.002  # kg/m² (convertido de 2g)
     potassio = 0.002  # kg/m² (convertido de 2g)
-    nitrogenio = 0  # kg/m² (convertido de 0g)
-    herbicida = 0.01  # mL/m²
-    fungicida = 0.01  # mL/m²
-    inseticida = 0.005  # mL/m²
+    nitrogenio = 0.001  # kg/m² (convertido de 1g)
+    herbicida = 0.01  # L/m²
+    fungicida = 0.01  # L/m²
+    inseticida = 0.005  # L/m²
     agua = 4  # L/m²/dia
 
     return {
+        "Mudas": f"{mudas} mudas",
         "Calcário": f"{formatar_numero(calcario * area_util)} Kg",
-        "Gesso": f"{formatar_numero(gesso * area_util)} Kg",
+        "Gesso Agrícola": f"{formatar_numero(gesso * area_util)} Kg",
         "Fosfato": f"{formatar_numero(fosfato * area_util)} Kg",
         "Potássio": f"{formatar_numero(potassio * area_util)} Kg",
         "Nitrogênio": f"{formatar_numero(nitrogenio * area_util)} Kg",
-        "Herbicida": f"{formatar_numero(herbicida * area_util)} mL",
-        "Fungicida": f"{formatar_numero(fungicida * area_util)} mL",
-        "Inseticida": f"{formatar_numero(inseticida * area_util)} mL",
+        "Herbicida": f"{formatar_numero(herbicida * area_util)} L",
+        "Fungicida": f"{formatar_numero(fungicida * area_util)} L",
+        "Inseticida": f"{formatar_numero(inseticida * area_util)} L",
         "Água": f"{formatar_numero(agua * area_util)} L por dia",
     }
 
@@ -46,9 +48,9 @@ def cal_insumos_cafe(area_util, area_total):
     potassio = 0.002  # kg/m² (convertido de 2g)
     calcario = 0.2  # kg/m² (convertido de 200g)
     gesso = 0.1  # kg/m² (convertido de 100g)
-    herbicida = 0.01  # mL/m²
-    fungicida = 0.01  # mL/m²
-    inseticida = 0.005  # mL/m²
+    herbicida = 0.01  # L/m²
+    fungicida = 0.01  # L/m²
+    inseticida = 0.005  # L/m²
     agua = 6  # L/m²/dia
 
     return {
@@ -58,8 +60,8 @@ def cal_insumos_cafe(area_util, area_total):
         "Potássio": f"{formatar_numero(potassio * area_util)} Kg",
         "Calcário": f"{formatar_numero(calcario * area_util)} Kg",
         "Gesso Agrícola": f"{formatar_numero(gesso * area_util)} Kg",
-        "Herbicida": f"{formatar_numero(herbicida * area_util)} mL",
-        "Fungicida": f"{formatar_numero(fungicida * area_util)} mL",
-        "Inseticida": f"{formatar_numero(inseticida * area_util)} mL",
+        "Herbicida": f"{formatar_numero(herbicida * area_util)} L",
+        "Fungicida": f"{formatar_numero(fungicida * area_util)} L",
+        "Inseticida": f"{formatar_numero(inseticida * area_util)} L",
         "Água": f"{formatar_numero(agua * area_util)} L por dia",
     }
